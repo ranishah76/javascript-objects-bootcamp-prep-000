0e2b6ddfb8090c9158ed30a931f01238d3d5bbfc
Rani <rani76@gmail.com>
2017-11-07 20:44:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,8 +1,8 @@
 
 var playlist = { artistName: songTitle };
 
-function updatePlaylist (playlist, artistName, songTitle)
-playlist[artistName] = songTitle
+function updatePlaylist (playlist, artistName, songTitle) {
+ playlist[artistName] = songTitle
 }
 
 function removeFromPlaylist (playlist, artistName) {
