0c341448ea584ec37e5f106c4014a8ac1296357d
Rani <rani76@gmail.com>
2017-11-07 20:21:51 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,14 @@
+var playlist = {
+'Phil Ochs': 'Heres to the State of Mississippi', 
+'Taylor Swift': 'Love Story'
+}
+
+
+function updatePlaylist (playlist, artistName, songTitle)
+  return playlist.assign ( {}, targetPlaylist, updatePlaylist )
+}
+
+function removeFromPlaylist (playlist, artistName, songTitle) {
+  delete.artistName.songTitle
+return playlist  
+}
